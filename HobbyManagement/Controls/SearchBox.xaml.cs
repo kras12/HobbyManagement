78703b51185451f29e06fa0e89769fa2554e18ae
runehero <runehero@hotmail.com>
2024-11-28 17:17:25 +0100

    Created a search box user control with a clear button and placed it above the hobbies list.

---
+++ HobbyManagement/Controls/SearchBox.xaml.cs
@@ -0,0 +1,41 @@
+﻿using HobbyManagement.Commands;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HobbyManagement.Controls
+{
+    /// <summary>
+    /// Interaction logic for SearchBox.xaml
+    /// </summary>
+    public partial class SearchBox : UserControl
+    {
+        public static readonly DependencyProperty SearchTextProperty = 
+            DependencyProperty.Register(nameof(SearchText), typeof(string), typeof(SearchBox), new PropertyMetadata(""));
+
+        public SearchBox()
+        {
+            InitializeComponent();
+        }
+
+        public ICommand ClearSearchCommand => new RelayCommand(ClearSearch, () => true);
+
+        public string SearchText
+        {
+            get
+            {
+                return (string)GetValue(SearchTextProperty);
+            }
+
+            set
+            {
+                SetValue(SearchTextProperty, value);
+            }
+        }
+
+        private void ClearSearch()
+        {
+            SearchText = "";
+        }
+    }
+}
