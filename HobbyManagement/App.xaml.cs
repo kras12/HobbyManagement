54c5aaaa24b1a115211956fbe9d2ccb793b06f4d
runehero <runehero@hotmail.com>
2024-11-29 14:24:11 +0100

    Minor refactoring.

--- HobbyManagement/App.xaml.cs
+++ HobbyManagement/App.xaml.cs
@@ -5,51 +5,50 @@ using HobbyManagement.Viewmodels;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
-namespace HobbyManagement
+namespace HobbyManagement;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private IServiceProvider _serviceProvider = default!;
+
+    protected override void OnStartup(StartupEventArgs e)
     {
-        private IServiceProvider _serviceProvider = default!;
+        base.OnStartup(e);
+        _serviceProvider = CreateServiceProvider();
+        _serviceProvider.GetRequiredService<MainWindow>().Show();
+    }
 
-        protected override void OnStartup(StartupEventArgs e)
+    private void ConfigureAutoMapper(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddTransient<IMapper>((service) =>
         {
-            base.OnStartup(e);
-            _serviceProvider = CreateServiceProvider();
-            _serviceProvider.GetRequiredService<MainWindow>().Show();
-        }
-
-        private void ConfigureAutoMapper(IServiceCollection serviceCollection)
-        {
-            serviceCollection.AddTransient<IMapper>((service) =>
+            var mapperConfig = new MapperConfiguration(x =>
             {
-                var mapperConfig = new MapperConfiguration(x =>
-                {
-                    x.AddProfile(new AutoMapperProfile());
-                    x.ConstructServicesUsing(x => service.GetRequiredService(x));
-                });
-
-                return mapperConfig.CreateMapper();
+                x.AddProfile(new AutoMapperProfile());
+                x.ConstructServicesUsing(x => service.GetRequiredService(x));
             });
-        }
 
-        private void ConfigureServices(IServiceCollection serviceCollection)
-        {
-            ConfigureAutoMapper(serviceCollection);
+            return mapperConfig.CreateMapper();
+        });
+    }
 
-            serviceCollection.AddSingleton<IHobbyManagerViewModel, HobbyManagerViewModel>();
-            serviceCollection.AddTransient<IHobbyViewModel, HobbyViewModel>();
-            serviceCollection.AddSingleton<IHobbyViewModelFactory, HobbyViewModelFactory>();
-            serviceCollection.AddTransient<MainWindow>();
-        }
+    private void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        ConfigureAutoMapper(serviceCollection);
 
-        private IServiceProvider CreateServiceProvider()
-        {
-            var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);
-            return serviceCollection.BuildServiceProvider();
-        }
+        serviceCollection.AddSingleton<IHobbyManagerViewModel, HobbyManagerViewModel>();
+        serviceCollection.AddTransient<IHobbyViewModel, HobbyViewModel>();
+        serviceCollection.AddSingleton<IHobbyViewModelFactory, HobbyViewModelFactory>();
+        serviceCollection.AddTransient<MainWindow>();
+    }
+
+    private IServiceProvider CreateServiceProvider()
+    {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        return serviceCollection.BuildServiceProvider();
     }
 }
