998f070c9cc726a82ca0b805e2569d9be3bdecb7
runehero <runehero@hotmail.com>
2024-12-03 11:04:23 +0100

    Moved the SearchBox control to its own namespace.

--- HobbyManagement/Controls/Search/SearchBox.xaml.cs
+++ HobbyManagement/Controls/Search/SearchBox.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace HobbyManagement.Controls
+namespace HobbyManagement.Controls.Search
 {
     /// <summary>
     /// Interaction logic for SearchBox.xaml
