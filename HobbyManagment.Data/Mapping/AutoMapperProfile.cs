a766384547a4c8ebe4cbc5187180629e3e601857
runehero <runehero@hotmail.com>
2024-12-05 10:25:43 +0100

    Created mappings between Hobby and HobbyEntity.

--- HobbyManagment.Data/Mapping/AutoMapperProfile.cs
+++ HobbyManagment.Data/Mapping/AutoMapperProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HobbyManagment.Data;
+using HobbyManagment.Data.Models;
 
 namespace HobbyManagement.Mapping;
 
@@ -8,5 +9,6 @@ class AutoMapperProfile : Profile
     public AutoMapperProfile()
     {
         CreateMap<Hobby, Hobby>();
+        CreateMap<HobbyEntity, Hobby>().ReverseMap();
     }
 }
